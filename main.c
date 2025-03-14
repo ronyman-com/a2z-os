35b5b0cf72d4ab2b31192e49cdc118bc2ec7f532
ronyman.com <47080351+ronyman-com@users.noreply.github.com>
2025-03-15 08:01:26 +1100

    Update main.c

--- main.c
+++ main.c
@@ -1,11 +1,15 @@
 #include "idt.h"
 #include "keyboard.h"
 #include "paging.h"
+#include "fs.h"
+
 
 void main() {
     idt_install();
     keyboard_install();
-     paging_install();
+    paging_install();
+    fs_init();
+    fs_create_file("test.txt", 10, "Hello A2Z");
     asm volatile("sti"); // Enable interrupts
     print_string("A2Z OS: Keyboard interrupt handling enabled!");
 }

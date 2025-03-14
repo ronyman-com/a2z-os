b9f6343cfeb4f25990a9452c903064b5d61eee82
ronyman.com <47080351+ronyman-com@users.noreply.github.com>
2025-03-15 08:13:24 +1100

    Update network.c

--- network.c
+++ network.c
@@ -1,2 +1,4 @@
 void send_packet(uint8_t* data, uint32_t size);
 void receive_packet(uint8_t* buffer);
+void ip_send(uint8_t* data, uint32_t size, uint32_t dest_ip);
+void tcp_send(uint8_t* data, uint32_t size, uint32_t dest_ip, uint16_t dest_port);

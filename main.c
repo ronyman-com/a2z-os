ccf7a58e685f146730e84ef06afe59b48ca27800
ronyman.com <47080351+ronyman-com@users.noreply.github.com>
2025-03-15 08:21:15 +1100

    Update main.c

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include "version.h"
 #include "idt.h"
 #include "keyboard.h"
 #include "paging.h"
@@ -15,6 +16,6 @@ void main() {
     shell_run();
     fs_create_file("test.txt", 10, "Hello A2Z");
     asm volatile("sti"); // Enable interrupts
-    print_string("A2Z OS: Keyboard interrupt handling enabled!");
+    print_string("A2Z Operator System v" A2Z_OS_VERSION " (" A2Z_OS_CODENAME ")");
 }
 

bb170614daf61c6b472d07e3e7dd67df3e1b53e1
ronyman.com <47080351+ronyman-com@users.noreply.github.com>
2025-03-15 08:05:08 +1100

    Update keyboard.c

--- keyboard.c
+++ keyboard.c
@@ -3,3 +3,16 @@ void keyboard_handler() {
     char c = scancode_to_ascii(scancode); // Convert scancode to ASCII
     print_character(c); // Print the character to the screen
 }
+
+void read_input(char* buffer) {
+    int i = 0;
+    while (1) {
+        char c = get_char(); // Get a character from keyboard input
+        if (c == '\n') {
+            buffer[i] = '\0';
+            break;
+        } else {
+            buffer[i++] = c;
+        }
+    }
+}

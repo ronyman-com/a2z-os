810f82ca024324558dee6aa4a361a96ed750f461
ronyman.com <47080351+ronyman-com@users.noreply.github.com>
2025-03-15 08:02:50 +1100

    Update main.c

--- main.c
+++ main.c
@@ -2,12 +2,14 @@
 #include "keyboard.h"
 #include "paging.h"
 #include "fs.h"
+#include "task.h"
 
 
 void main() {
     idt_install();
     keyboard_install();
     paging_install();
+    task_switch(); // Example of task switching
     fs_init();
     fs_create_file("test.txt", 10, "Hello A2Z");
     asm volatile("sti"); // Enable interrupts
